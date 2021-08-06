950784fd6e73abdf3044674fd9a01096b6ec65a8
Xa9aX ツ <mishradiganta91@gmail.com>
2021-08-07 01:22:00 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import math
-from .evornorm2d import EvoNorm2D
+from .evonorm2d import EvoNorm2D
 
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
